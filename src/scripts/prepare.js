fa481141e88efbff484f0a5575bafd7f364b872d
Dimitar Todorov <Dimitar.Todorov@progress.com>
2018-08-22 13:35:59 +0300

    chore: add regexp match for tns version

--- src/scripts/prepare.js
+++ src/scripts/prepare.js
@@ -8,9 +8,10 @@ exec('tns --version', (err, stdout, stderr) => {
         return;
     }
 
-    console.log("------- " + stdout);
-    console.log("------- coerce " + semver.coerce(stdout));
-    const tnsVersion = semver.major(semver.coerce(stdout));
+    const regex = /^(?:\d+\.){2}\d+.*?$/m;
+    const matches = stdout.match(regex);
+    console.log("Extracted tns version: " + matches[0]);
+    const tnsVersion = semver.major(semver.coerce(matches[0]));
 
     // execute 'tns plugin build' for {N} version > 4. This command builds .aar in platforms/android folder.
     if (tnsVersion >= 4) {
